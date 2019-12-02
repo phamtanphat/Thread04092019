429ae2214d1ae5801ce022720bf5e5af69fe83da
phatdroid94 <phatdroid94@gmail.com>
2019-12-02 21:27:33 +0700

    finish

--- app/src/main/java/com/example/thread04092019/MainActivity.java
+++ app/src/main/java/com/example/thread04092019/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thread04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 //        Luong1 luong1 = new Luong1();
 //        luong1.run();
 
-        Thread a = new Thread(new Runnable() {
+        final Thread a = new Thread(new Runnable() {
             @Override
             public void run() {
                 handleThread(" A ");
@@ -35,14 +36,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 handleThread(" B ");
+
             }
         });
+        // 1 : random A
+        // 2 : random b
+        // 3 : random c
 
         a.start();
         b.start();
+
     }
-    private synchronized void handleThread(String key){
-        for (int i = 0 ; i < 100 ; i++){
+    // deadlock : 2 thang cung 1 cong viec ma khong dong bo thi se dan deadlock
+    private void handleThread(String key){
+        for (int i = 0 ; i < 1000 ; i++){
             Log.d("BBB",key + i);
         }
     }
