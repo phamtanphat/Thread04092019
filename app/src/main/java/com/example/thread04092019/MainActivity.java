0003eb13b80f21a584be9a15ce71c21d33e93fbf
phatdroid94 <phatdroid94@gmail.com>
2019-12-04 20:48:43 +0700

    finish

--- app/src/main/java/com/example/thread04092019/MainActivity.java
+++ app/src/main/java/com/example/thread04092019/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     int inta = 0;
     int intb = 0;
     int intc = 0;
-    Laco laco;
+    Laco laco = new Laco(0);
     Thread a, b, c;
 
     @Override
@@ -29,19 +29,21 @@ public class MainActivity extends AppCompatActivity {
         // 2: Tao ra object ket thua runble
 //        Luong1 luong1 = new Luong1();
 //        luong1.run();
-        laco = new Laco(0);
+
         a = new Thread(new Runnable() {
             @Override
             public void run() {
                 synchronized (laco){
-                    for (int i = 0; i < 50; i++) {
+                    for (int i = 0; i < 1000; ) {
                         if (laco.count == 0) {
                             inta = i;
-                            laco.count = 1;
                             Log.d("BBB", " Gia tri cua A : " + inta + " - vi tri " + i);
+                            i++;
+                            laco.count = 1;
+                            laco.notifyAll();
                         } else {
                             try {
-                                a.wait();
+                                laco.wait();
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -54,14 +56,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 synchronized (laco){
-                    for (int i = 0; i < 50; i++) {
+                    for (int i = 0; i < 1000;) {
                         if (laco.count == 1) {
                             intb = i;
                             laco.count = 2;
                             Log.d("BBB", " Gia tri cua B : " + intb + " - vi tri " + i);
+                            i++;
+                            laco.notifyAll();
                         } else {
                             try {
-                                b.wait();
+                                laco.wait();
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -75,14 +79,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 synchronized (laco){
-                    for (int i = 0; i < 50; i++) {
+                    for (int i = 0; i < 1000; ) {
                         if (laco.count == 2) {
                             intc = inta + intb;
                             laco.count = 0;
                             Log.d("BBB", " Gia tri cua C : " + intc + " - vi tri " + i);
+                            i++;
+                            laco.notifyAll();
                         } else {
                             try {
-                                c.wait();
+                                laco.wait();
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -97,10 +103,5 @@ public class MainActivity extends AppCompatActivity {
         c.start();
 
     }
-    // deadlock : 2 thang cung 1 cong viec ma khong dong bo thi se dan deadlock
-//    private void handleThread(String key){
-//        for (int i = 0 ; i < 1000 ; i++){
-//            Log.d("BBB",key + i);
-//        }
-//    }
+
 }
