a5872db0c66d0cc7de16c01ea272ce68fca1d655
phatdroid94 <phatdroid94@gmail.com>
2019-12-04 20:28:35 +0700

    dong bo thread

---
+++ app/src/main/java/com/example/thread04092019/Laco.java
@@ -0,0 +1,9 @@
+package com.example.thread04092019;
+
+public class Laco {
+    Integer count;
+
+    public Laco(Integer count) {
+        this.count = count;
+    }
+}
--- app/src/main/java/com/example/thread04092019/MainActivity.java
+++ app/src/main/java/com/example/thread04092019/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     int inta = 0;
     int intb = 0;
     int intc = 0;
+    Laco laco;
+    Thread a, b, c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,33 +29,68 @@ public class MainActivity extends AppCompatActivity {
         // 2: Tao ra object ket thua runble
 //        Luong1 luong1 = new Luong1();
 //        luong1.run();
-        Thread a = new Thread(new Runnable() {
+        laco = new Laco(0);
+        a = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0 ; i < 50 ; i++){
-                    inta = i;
-                    Log.d("BBB"," Gia tri cua A : " + inta + " - vi tri " + i);
+                synchronized (laco){
+                    for (int i = 0; i < 50; i++) {
+                        if (laco.count == 0) {
+                            inta = i;
+                            laco.count = 1;
+                            Log.d("BBB", " Gia tri cua A : " + inta + " - vi tri " + i);
+                        } else {
+                            try {
+                                a.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
-        Thread b = new Thread(new Runnable() {
+        b = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0 ; i < 50 ; i++){
-                    intb = i;
-                }
-            }
-        });
-        Thread c = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0 ; i < 50 ; i++){
-                    intc = inta + intb;
+                synchronized (laco){
+                    for (int i = 0; i < 50; i++) {
+                        if (laco.count == 1) {
+                            intb = i;
+                            laco.count = 2;
+                            Log.d("BBB", " Gia tri cua B : " + intb + " - vi tri " + i);
+                        } else {
+                            try {
+                                b.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
 
             }
         });
-
+        c = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (laco){
+                    for (int i = 0; i < 50; i++) {
+                        if (laco.count == 2) {
+                            intc = inta + intb;
+                            laco.count = 0;
+                            Log.d("BBB", " Gia tri cua C : " + intc + " - vi tri " + i);
+                        } else {
+                            try {
+                                c.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
 
         a.start();
         b.start();
